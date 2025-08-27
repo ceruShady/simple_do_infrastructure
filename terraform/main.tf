558823f229e14bd5dd9c90934c22c8db26f62e27
ceruShady <qiop1397@gmail.com>
2025-08-27 11:48:33 +0800

    Prep work to add provisioning for DO spaces

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,17 @@
 resource "digitalocean_droplet" "master" {
   name     = "master"
-  image    = "ubuntu-24-04-x64"
-  region   = "sgp1"
-  size     = "s-1vcpu-1gb"
+  image    = var.image_master
+  region   = var.do_region
+  size     = var.size_master
   ssh_keys = [digitalocean_ssh_key.admin_ssh_key.id]
 }
 
 resource "digitalocean_droplet" "workers" {
   count    = var.worker_num
   name     = "worker-${count.index}"
-  image    = "ubuntu-24-04-x64"
-  region   = "sgp1"
-  size     = "s-1vcpu-1gb"
+  image    = var.image_worker
+  region   = var.do_region
+  size     = var.size_worker
   ssh_keys = [digitalocean_ssh_key.admin_ssh_key.id]
 }
 
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,3 +1,28 @@
+variable "do_region" {
+  description = "Region for Digital Ocean resources"
+  default     = "sgp1"
+}
+
+variable "image_master" {
+  description = "Image for master instance"
+  default     = "ubuntu-24-04-x64"
+}
+
+variable "image_worker" {
+  description = "Image for worker instance"
+  default     = "ubuntu-24-04-x64"
+}
+
+variable "size_master" {
+  description = "Size configuration for master"
+  default     = "s-1vcpu-1gb"
+}
+
+variable "size_worker" {
+  description = "Size configuration for worker"
+  default     = "s-1vcpu-1gb"
+}
+
 variable "do_token" {
   description = "Digital Ocean token"
 }
@@ -5,6 +30,14 @@ variable "do_token" {
 variable "ssh_key_location" {
   description = "Location of ssh public key"
   default     = "./secrets/do-admin-key.pub"
+}
+
+variable "do_spaces_access" {
+  description = "Access ID for Digital Ocean Spaces"
+}
+
+variable "do_spaces_secrets" {
+  description = "Secret key for Digital Ocean Spaces"
 }
 
 variable "worker_num" {
