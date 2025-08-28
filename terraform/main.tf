1e3fc386348f3768c459f3929f0227e9521b14b6
ceruShady <qiop1397@gmail.com>
2025-08-28 12:46:34 +0800

    Change main.tf to generate SSH key and ansible inventory file

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,8 +32,39 @@ resource "digitalocean_firewall" "cluster_firewall" {
   join(", ", [for i in digitalocean_droplet.workers : i.id])]
 }
 
+resource "local_file" "inventory" {
+  filename = "../ansible/inventory.yaml"
+  content = templatefile("./templates/inventory.yaml.tftpl", {
+    master_ip       = digitalocean_droplet.master.ipv4_address
+    ssh_private_key = digitalocean_ssh_key.admin_ssh_key.fingerprint
+    #master_domain = "code-server-${digitalocean_droplet.master.ipv4_address}.nip.io"
+  })
+  file_permission = "0444"
+}
+
+# Generate SSH key
+resource "tls_private_key" "ssh_key_pair" {
+  algorithm = "ED25519"
+}
+
+# Reference generated SSH key
 resource "digitalocean_ssh_key" "admin_ssh_key" {
   name       = "do-admin-key"
-  public_key = sensitive(file(var.ssh_key_location))
+  public_key = sensitive(tls_private_key.ssh_key_pair.public_key_openssh)
 }
+
+# Output generated SSH key
+#output "private_key_openssh" {
+#  value     = tls_private_key.ssh_key_pair.private_key_openssh
+#  sensitive = true
+#}
+
+# Save generated SSH private key
+resource "local_file" "private_key_file" {
+  content         = tls_private_key.ssh_key_pair.private_key_openssh
+  filename        = "./secrets/my_droplet_key.pem"
+  file_permission = "0600"
+}
+
+
 
