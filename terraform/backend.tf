29012b09273b73c37bcff1b8e1993dd6ffb284fc
ceruShady <qiop1397@gmail.com>
2025-09-01 12:23:13 +0800

    Enable state lock, commented fields for sensitive information

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,19 +1,24 @@
 terraform {
   backend "s3" {
-endpoints = {
+    endpoints = {
       s3 = "https://sgp1.digitaloceanspaces.com"
     }
 
-    bucket     = "my-do-bucket" #var.do_spaces_bucket_name
-    key        = "terraform.tfstate" #var.terraform_state_filename
-    region     = "sgp1" #var.do_region
-    access_key = #var.do_spaces_access
-    secret_key = #var.do_spaces_secrets
-    encrypt    = true
+    # Use GitHub Actions secrets to insert sensitive information
+    # e.g. terraform init -backend-config="bucket=${{ secrets.TF_STATE_BUCKET }}"
+    #bucket     = "<YOUR_BUCKET_NAME>" 
+    key = "terraform.tfstate"
+    #access_key = "<YOUR_BUCKET_ACCESS_KEY>" 
+    #secret_key = "<YOUR_BUCKET_SECRET_KEY>" 
+    encrypt = true
+
     skip_credentials_validation = true
     skip_requesting_account_id  = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
     skip_s3_checksum            = true
+    region                      = "ap-southeast-1" #var.do_region
+
+    use_lockfile = true
   }
 }
