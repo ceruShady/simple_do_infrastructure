76fc207e3c2e18b5670d86d12d7e21c38a915b69
ceruShady <qiop1397@gmail.com>
2025-09-03 11:58:00 +0800

    Changed reference for SSH details to use variable instead of generating one on the spot

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,15 +49,26 @@ resource "local_file" "inventory" {
 }
 
 # Generate SSH key
-resource "tls_private_key" "ssh_key_pair" {
-  algorithm = "ED25519"
-}
+#resource "tls_private_key" "ssh_key_pair" {
+#  algorithm = "ED25519"
+#}
 
 # Reference generated SSH key
+#resource "digitalocean_ssh_key" "admin_ssh_key" {
+#  name       = "do-admin-key"
+#  public_key = sensitive(tls_private_key.ssh_key_pair.public_key_openssh)
+#}
+
+# Reference SSH public
 resource "digitalocean_ssh_key" "admin_ssh_key" {
   name       = "do-admin-key"
-  public_key = sensitive(tls_private_key.ssh_key_pair.public_key_openssh)
+  public_key = sensitive(var.ssh_pub_key)
 }
+
+# Referencing to SSH uploaded in DigitalOcean
+#resource "digitalocean_ssh_key" "existing_key" {
+#  name = "<NAME_OF_SSH_KEY>" # Or use fingerprint = "your_key_fingerprint"
+#}
 
 # Output generated SSH key
 #output "private_key_openssh" {
@@ -66,11 +77,11 @@ resource "digitalocean_ssh_key" "admin_ssh_key" {
 #}
 
 # Save generated SSH private key
-resource "local_file" "private_key_file" {
-  content         = tls_private_key.ssh_key_pair.private_key_openssh
-  filename        = "./secrets/my_droplet_key.pem"
-  file_permission = "0600"
-}
+#resource "local_file" "private_key_file" {
+#  content         = tls_private_key.ssh_key_pair.private_key_openssh
+#  filename        = "./secrets/my_droplet_key.pem"
+#  file_permission = "0600"
+#}
 
 
 
