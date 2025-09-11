8d5b2f99e7a1ec3ddcab52ddc105804259cc61a1
ceruShady <qiop1397@gmail.com>
2025-09-11 10:39:56 +0800

    Changed size of resources, added variable for location of ansible inventory template, comment out variable for location of ssh public key file

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -10,10 +10,10 @@ variable "do_token" {
   description = "Digital Ocean token"
 }
 
-variable "ssh_key_location" {
-  description = "Location of ssh public key"
-  default     = "./secrets/do-admin-key.pub"
-}
+#variable "ssh_key_location" {
+#  description = "Location of ssh public key"
+#  default     = "./secrets/do-admin-key.pub"
+#}
 
 variable "do_region" {
   description = "Region for Digital Ocean resources"
@@ -37,12 +37,12 @@ variable "image_worker" {
 
 variable "size_master" {
   description = "Size configuration for master"
-  default     = "s-1vcpu-2gb"
+  default     = "s-2vcpu-2gb"
 }
 
 variable "size_worker" {
   description = "Size configuration for worker"
-  default     = "s-1vcpu-1gb"
+  default     = "s-1vcpu-2gb"
 }
 
 # DO spaces variables
@@ -78,6 +78,11 @@ variable "ansible_inventory_location" {
 variable "ansible_inventory_content_type" {
   description = "MIME type of Ansible inventory file"
   default     = "application/yaml"
+}
+
+variable "ansible_inventory_template_location" {
+  description = "Location of template for Ansible inventory file"
+  default     = "./templates/inventory.yaml.tftpl"
 }
 
 variable "ansible_user_name" {
