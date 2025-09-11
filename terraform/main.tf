ea78ed72f240f36c2912defcbb9f777ed8e9b334
ceruShady <qiop1397@gmail.com>
2025-09-11 10:42:40 +0800

    Removed all references to resources no longer in use

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# Provisioned resources
 resource "digitalocean_droplet" "master" {
   name     = "master"
   image    = var.image_master
@@ -16,55 +17,3 @@ resource "digitalocean_droplet" "workers" {
   ssh_keys = [digitalocean_ssh_key.admin_ssh_key.id]
   tags     = ["worker"]
 }
-
-#resource "digitalocean_vpc" "k8s_vpc" {
-#  name     = var.vpc_name
-#  region   = var.region
-#  ip_range = "10.1.0.0/16"
-#}
-
-output "worker_ids" {
-  value = digitalocean_droplet.workers.*.id
-}
-
-output "worker_ids_arr" {
-  value = join(", ", [for i in digitalocean_droplet.workers : i.id])
-}
-
-# Generate SSH key
-#resource "tls_private_key" "ssh_key_pair" {
-#  algorithm = "ED25519"
-#}
-
-# Reference generated SSH key
-#resource "digitalocean_ssh_key" "admin_ssh_key" {
-#  name       = "do-admin-key"
-#  public_key = sensitive(tls_private_key.ssh_key_pair.public_key_openssh)
-#}
-
-# Reference SSH public
-resource "digitalocean_ssh_key" "admin_ssh_key" {
-  name       = "do-admin-key"
-  public_key = sensitive(var.ssh_pub_key)
-}
-
-# Referencing to SSH uploaded in DigitalOcean
-#resource "digitalocean_ssh_key" "existing_key" {
-#  name = "<NAME_OF_SSH_KEY>" # Or use fingerprint = "your_key_fingerprint"
-#}
-
-# Output generated SSH key
-#output "private_key_openssh" {
-#  value     = tls_private_key.ssh_key_pair.private_key_openssh
-#  sensitive = true
-#}
-
-# Save generated SSH private key
-#resource "local_file" "private_key_file" {
-#  content         = tls_private_key.ssh_key_pair.private_key_openssh
-#  filename        = "./secrets/my_droplet_key.pem"
-#  file_permission = "0600"
-#}
-
-
-
