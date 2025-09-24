bf78c62aec6e95e2141c778b439ca58a5692eaaa
ceruShady <qiop1397@gmail.com>
2025-09-24 12:55:31 +0800

    Enabled provision of load balancer

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -1,44 +1,31 @@
-#resource "digitalocean_certificate" "cert" {
-#  name             = "cert"
-#  private_key      = "file('key.pem')"
-#  leaf_certificate = "file('cert.pem')"
+resource "digitalocean_loadbalancer" "public" {
+  name        = "kube-load-balancer"
+  region      = var.do_region
+  size        = "lb-small"
+  size_unit   = 1
+  vpc_uuid    = digitalocean_vpc.do_vpc.id
+  droplet_tag = digitalocean_tag.tag_master.id
 
-#  lifecycle {
-#    create_before_destroy = true
-#  }
-#}
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
 
-#resource "digitalocean_loadbalancer" "public" {
-#  name      = "loadbalancer-1"
-#  region    = var.do_region
-#  size      = "lb-small"
-#  size_unit = 1
-#  vpc_uuid  = digitalocean_vpc.do_vpc.id
+    target_port     = 80
+    target_protocol = "http"
+  }
 
-#  forwarding_rule {
-#    entry_port     = 80
-#    entry_protocol = "http"
+  #  forwarding_rule {
+  #    entry_port     = 443
+  #    entry_protocol = "https"
 
-#    target_port     = 80
-#    target_protocol = "http"
+  #    target_port     = 443
+  #    target_protocol = "https"
 
-#certificate_name = digitalocean_certificate.cert.name
-#  }
+  #    tls_passthrough = true
+  #  }
 
-#  forwarding_rule {
-#    entry_port     = 443
-#    entry_protocol = "https"
-
-#    target_port     = 443
-#    target_protocol = "https"
-
-#    tls_passthrough = true
-#  }
-
-#  healthcheck {
-#    port     = 22
-#    protocol = "tcp"
-#  }
-
-#  droplet_tag = digitalocean_tag.tag_master.id
-#}
+  healthcheck {
+    port     = 22
+    protocol = "tcp"
+  }
+}
